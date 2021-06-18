a6f8a4ec4ad0849a1f0494fbb33ec0c531753435
bayudarupangestu <bayudp22@gmail.com>
2021-06-18 21:45:02 +0700

    Update table event

--- migrations/1623506971374_create-table-events.js
+++ migrations/1623506971374_create-table-events.js
@@ -2,13 +2,23 @@
 
 exports.shorthands = undefined;
 
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ */
+
 exports.up = (pgm) => {
-  pgm.createTable('events', {
-    id: { type: 'VARCHAR(16)' },
-    name: { type: 'TEXT' },
-  });
+    pgm.createTable('events', {
+        id: { type: 'VARCHAR(50)', notNull: true, primaryKey: true },
+        id_ormawa: { type: 'VARCHAR(50)', notNull: true },
+        judul: { type: 'TEXT', notNull: true },
+        deskripsi: { type: 'TEXT', notNull: true },
+        url_foto_pamflet: { type: 'TEXT', notNull: true },
+        url_pendaftaran: { type: 'TEXT', notNull: true },
+        waktu_acara: { type: 'TIMESTAMP', notNull: true },
+        waktu_ditambah: { type: 'TIMESTAMP', notNull: true },
+    });
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable('events');
+    pgm.dropTable('events');
 };
