6982ea464b4a4e3164c73ba35226bca019adb9c1
bayudarupangestu <bayudp22@gmail.com>
2021-06-19 20:19:12 +0700

    Update table event

--- migrations/1623506971374_create-table-events.js
+++ migrations/1623506971374_create-table-events.js
@@ -7,18 +7,18 @@ exports.shorthands = undefined;
  */
 
 exports.up = (pgm) => {
-    pgm.createTable('events', {
-        id: { type: 'VARCHAR(50)', notNull: true, primaryKey: true },
-        id_ormawa: { type: 'VARCHAR(50)', notNull: true },
-        judul: { type: 'TEXT', notNull: true },
-        deskripsi: { type: 'TEXT', notNull: true },
-        url_foto_pamflet: { type: 'TEXT', notNull: true },
-        url_pendaftaran: { type: 'TEXT', notNull: true },
-        waktu_acara: { type: 'TIMESTAMP', notNull: true },
-        waktu_ditambah: { type: 'TIMESTAMP', notNull: true },
-    });
+  pgm.createTable('events', {
+    id: { type: 'VARCHAR(50)', notNull: true, primaryKey: true },
+    id_ormawa: { type: 'VARCHAR(50)', notNull: true },
+    judul: { type: 'TEXT', notNull: true },
+    deskripsi: { type: 'TEXT', notNull: true },
+    url_foto_pamflet: { type: 'TEXT', notNull: true },
+    url_pendaftaran: { type: 'TEXT', notNull: true },
+    waktu_acara: { type: 'TIMESTAMP', notNull: true },
+    waktu_ditambah: { type: 'TIMESTAMP', notNull: true },
+  });
 };
 
 exports.down = (pgm) => {
-    pgm.dropTable('events');
+  pgm.dropTable('events');
 };
